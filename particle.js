af4dc18a64138bd95f412a4439d102840d1bc301
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2015-03-19 10:31:39 -0700

    BG

--- particle.js
+++ particle.js
@@ -7,8 +7,8 @@
 
   var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setClearColor(new THREE.Color(0, 1));
+  var renderer = new THREE.WebGLRenderer({alpha: true});
+  renderer.setClearColor(0x000000, 0);
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.getElementById("bg").appendChild( renderer.domElement );
 
